3272511fd3c28c2415dd02cc7f085544b6da4c97
Alexandre Juca <corextechnologies@gmail.com>
2020-02-17 12:03:44 +0100

    Added functions to generate table from struct metdata

--- helper.go
+++ helper.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"reflect"
 	"runtime"
 	"strings"
 
+	"github.com/alexeyco/simpletable"
 	"github.com/mgutz/ansi"
 )
 
@@ -83,4 +85,104 @@ func Loading() {
 	fmt.Println(PrintColoredDashes(108, "cyan"))
 	fmt.Println(PrintColoredText("Loading...", "cyan"))
 	fmt.Println(PrintColoredDashes(108, "cyan"))
+}
+
+// GetTypeAttributes - Get Fields
+func GetTypeAttributes(azureResource interface{}) (reflect.Type, int) {
+	convertedType := reflect.ValueOf(azureResource).Elem()
+	return convertedType.Type(), convertedType.NumField()
+}
+
+// GetAllFieldsAndValues - Get All Fields And Values
+func GetAllFieldsAndValues(azureResource interface{}) ([]string, []string) {
+	var fields []string
+	var values []string
+
+	resType, size := GetTypeAttributes(azureResource)
+	s := reflect.ValueOf(azureResource).Elem()
+	for i := 0; i < size; i++ {
+		field := resType.Field(i).Name
+		value := fmt.Sprintf("%v", s.Field(i).Interface())
+		fields = append(fields, field)
+		values = append(values, value)
+	}
+	return fields, values
+}
+
+// GenerateCellsForHeader -
+func GenerateCellsForHeader(fields []string, color string) []*simpletable.Cell {
+	var cells []*simpletable.Cell
+	for i := 0; i < len(fields); i++ {
+		field := fields[i]
+		cell := simpletable.Cell{Align: simpletable.AlignLeft, Text: PrintColoredText(field, color)}
+		cells = append(cells, &cell)
+	}
+	return cells
+}
+
+// GenerateCellsForBody - Generate Cells for body
+func GenerateCellsForBody(values []string, size int) []*simpletable.Cell {
+	var cells []*simpletable.Cell
+	for i := 0; i < size; i++ {
+		var cell simpletable.Cell
+		if i == 0 {
+			cell = simpletable.Cell{Align: simpletable.AlignLeft, Text: fmt.Sprintf(values[i][0:30])}
+		} else {
+			cell = simpletable.Cell{Align: simpletable.AlignLeft, Text: fmt.Sprintf(values[i])}
+		}
+		cells = append(cells, &cell)
+	}
+	return cells
+}
+
+// ShowTable - Show Table Contents
+func ShowTable(fields []string, values []string) {
+	table := simpletable.New()
+	var cells []*simpletable.Cell
+
+	for i := 0; i < len(fields); i++ {
+		cells = GenerateCellsForHeader(fields, "yellow")
+	}
+
+	table.Header = &simpletable.Header{
+		Cells: cells,
+	}
+
+	var cellsForBody []*simpletable.Cell
+	for i := 0; i < len(fields); i++ {
+		cellsForBody = GenerateCellsForBody(values, len(fields))
+		table.Body.Cells = append(table.Body.Cells, cellsForBody)
+	}
+
+	table.SetStyle(simpletable.StyleDefault)
+	fmt.Println(table.String())
+}
+
+// GetResourceMetada - Get struct information via reflection
+func GetResourceMetada(resourceGroups interface{}) ([]string, []string) {
+	return GetAllFieldsAndValues(resourceGroups)
+}
+
+// DisplayTable - Show Table Contents
+func displayTable(resourceGroups interface{}) {
+	table := simpletable.New()
+	fields, values := GetAllFieldsAndValues(resourceGroups)
+	var cells []*simpletable.Cell
+
+	for i := 0; i < len(fields); i++ {
+		cells = GenerateCellsForHeader(fields, "yellow")
+	}
+
+	table.Header = &simpletable.Header{
+		Cells: cells,
+	}
+
+	var cellsForBody []*simpletable.Cell
+	for i := 0; i < len(values); i++ {
+		cellsForBody = GenerateCellsForBody(values, len(values))
+		table.Body.Cells = append(table.Body.Cells, cellsForBody)
+	}
+
+	table.SetStyle(simpletable.StyleDefault)
+	fmt.Println(table.String())
 }
